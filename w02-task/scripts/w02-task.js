306846a9fc4a54ad51aac6da609da6a55e40be3c
Creed <creedmatu44@gmail.com>
2023-09-23 23:23:20 -0600

    hmmm

---
+++ w02-task/scripts/w02-task.js
@@ -0,0 +1,32 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+
+
+/* Step 2 - Variables */
+
+let fullName = `Creed Matuauto`;
+currentYear = Date();
+profilePicture = 'images/waiola.jpeg';
+
+/* Step 3 - Element Variables */
+
+
+
+
+
+/* Step 4 - Adding Content */
+
+
+
+
+
+
+/* Step 5 - Array */
+
+
+
+
+
+
